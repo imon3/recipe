4229494fd8fdbe5d1e2f85088c3075128a480dac
Imon Ovbude <imonovbude@gmail.com>
2019-06-14 16:57:51 -0700

    completed RecipeDetaail component and setup switch case in App.js to render components

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { recipes } from "./tempFiles/tempList";
+import { recipeDetails } from "./tempFiles/tempDetails";
 import RecipeList from "./components/recipes/RecipeList";
 import RecipeDetails from "./components/recipes/RecipeDetails";
 
@@ -11,34 +12,46 @@ class App extends React.Component {
     this.state = {
       recipes: recipes,
       url:
-        "https://www.food2fork.com/api/search?key=f141ed8cccedb28ecdcabc6507092a51"
+        "https://www.food2fork.com/api/search?key=f141ed8cccedb28ecdcabc6507092a51",
+      details_id: 35387,
+      pageIndex: 0
     };
   }
 
-  // async getRecipes() {
-  //   try {
-  //     const data = await fetch(this.state.url);
-  //     const jsonData = await data.json();
-  //     this.setState({
-  //       recipes: jsonData.recipes
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // componentDidMount() {
-  //   this.getRecipes();
-  // }
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1:
+        return <RecipeList recipes={this.state.recipes} />;
+      case 0:
+        return (
+          <RecipeDetails
+            id={this.state.details_id}
+            recipeDetails={recipeDetails}
+          />
+        );
+    }
+  };
 
   render() {
     // console.log(this.state.recipes);
-    return (
-      <>
-        <RecipeList recipes={this.state.recipes} />
-        <RecipeDetails />
-      </>
-    );
+    return <>{this.displayPage(this.state.pageIndex)}</>;
   }
 }
 
