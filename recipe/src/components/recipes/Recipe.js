bfc5d66b24382f6c9bf8ab011a5b5b27007c3c81
Imon Ovbude <imonovbude@gmail.com>
2019-06-14 16:04:51 -0700

    completed component for Recipe.js

--- recipe/src/components/recipes/Recipe.js
+++ recipe/src/components/recipes/Recipe.js
@@ -2,9 +2,44 @@ import React from "react";
 
 class Recipe extends React.Component {
   render() {
+    const {
+      image_url,
+      title,
+      publisher,
+      source_url,
+      recipe_id
+    } = this.props.recipe;
+
     return (
       <>
-        <h1>Hello from Recipe</h1>
+        <div className="col-10 mx-auto col-md-6 col-lg-4 my-3">
+          <div className="card">
+            <img
+              src={image_url}
+              alt="recipe"
+              className="img-card-top img-height"
+            />
+            <div className="card-body text-capitalize">
+              <h6>{title}</h6>
+              <h6 className="text-warning text-slanted">
+                provided by {publisher}
+              </h6>
+            </div>
+            <div className="card-footer">
+              <button type="button" className="btn btn-primary text-capitalize">
+                details
+              </button>
+              <a
+                href={source_url}
+                className="btn btn-success mx-2 text-capitalize"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                recipe url
+              </a>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
